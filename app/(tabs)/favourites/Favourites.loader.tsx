3074d58abf4556baa93904aa35159951e57e7330
danlaurent <daniel.s.laurent@gmail.com>
2024-02-18 16:09:14 +0100

    refactor: code cleaning

--- app/(tabs)/favourites/Favourites.loader.tsx
+++ app/(tabs)/favourites/Favourites.loader.tsx
@@ -1,5 +1,4 @@
 import { Favourites } from "./Favourites";
-import { useGetPastLaunchesQuery } from "../../../src/services/spaceX";
 import { Layout } from "../../../src/components/Layout";
 import { useFavourites } from "../../../src/hooks/useFavourites";
 
