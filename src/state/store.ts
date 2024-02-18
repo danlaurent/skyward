fb7e1d63137584d6e3776fa2e75b1a879e5478e7
danlaurent <daniel.s.laurent@gmail.com>
2024-02-18 15:46:24 +0100

    feat: create favourites reducer

--- src/state/store.ts
+++ src/state/store.ts
@@ -1,17 +1,33 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { spaceXApi } from "../services/spaceX";
 import { setupListeners } from "@reduxjs/toolkit/query";
+import favouritesReducer from "./slices/favouritesSlice";
+import storage from "redux-persist/lib/storage";
+import { persistReducer, persistStore } from "redux-persist";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+const persistConfig = {
+  key: "root",
+  storage: AsyncStorage,
+};
+
+const persistedReducer = persistReducer(persistConfig, favouritesReducer);
 
 export const store = configureStore({
   reducer: {
     [spaceXApi.reducerPath]: spaceXApi.reducer,
+    favourites: persistedReducer,
   },
   middleware: (getDefaultMiddleware) => {
-    return getDefaultMiddleware().concat(spaceXApi.middleware);
+    return getDefaultMiddleware({ serializableCheck: false }).concat(
+      spaceXApi.middleware
+    );
   },
 });
 
 setupListeners(store.dispatch);
+
+export const persistor = persistStore(store);
 
 export type AppDispatch = typeof store.dispatch;
 export type StoreType = typeof store;
